ad262b50ab084b621455155eb086f0ce6c456019
poemdexter <poemdexter@gmail.com>
2013-10-28 20:58:44 -0500

    java configuration version of connection

--- src/main/java/com/poemdexter/App.java
+++ src/main/java/com/poemdexter/App.java
@@ -1,23 +1,16 @@
 package com.poemdexter;
 
 
-import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
-		ConnectionFactory connectionFactory = new CachingConnectionFactory();
+		ApplicationContext context = new AnnotationConfigApplicationContext(RabbitConfiguration.class);
+		AmqpTemplate template = context.getBean(AmqpTemplate.class);
 
-		AmqpAdmin admin = new RabbitAdmin(connectionFactory);
-		admin.declareQueue(new Queue("myqueue"));
-
-		AmqpTemplate template = new RabbitTemplate(connectionFactory);
 		template.convertAndSend("myqueue", "foo");
 
 		String foo = (String) template.receiveAndConvert("myqueue");
